ed1cfed2b4d89e78be9423ee69f7e1f6e2881411
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-07-05 01:39:55 -0500

    no funciona

--- app/src/main/java/com/rabbitimpulsorademercados/gestionservicios/mensaje.kt
+++ app/src/main/java/com/rabbitimpulsorademercados/gestionservicios/mensaje.kt
@@ -10,12 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class mensaje : AppCompatActivity() {
-
+    private lateinit var timerLock: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mensaje)
 
         val intent = Intent(this, Service::class.java)
+        //Llama al temporizador para bloquear el dispositivo.
+        temporizadorLock()
         //Se llama al servicio donde pone a trabajar e sersor de proximidad aun que la tablet este apagada.
         startService(intent)
 
@@ -24,6 +26,27 @@ class mensaje : AppCompatActivity() {
 
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+
+    }
+
+    //Temporizador para llamar servicio de bloqueo.
+    fun temporizadorLock(){
+        val cargarLock = Intent(this, ServiceLock::class.java) //para recargar activity
+
+        //En la variable tiempoSegundos se ingresa la cantidad de tiempo para que se recargue la activity.
+        val tiempoSegundos = 12
+        val tiempoMilisegundos = (tiempoSegundos.toLong())*1000
+
+
+        timerLock = object : CountDownTimer(tiempoMilisegundos, 1000) {
+            override fun onTick(p0: Long) {
+                val cuentaSegundos = (p0/1000).toInt()
+            }
+            override fun onFinish() {
+                //Inicia el servicio de bloqueo.
+                //startService(cargarLock)
+            }
+        }.start()
 
     }
 
